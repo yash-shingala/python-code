f995c152846a85c1f38b99870fa7efe091a4e9c0
yash-shingala <yash.shingala.cg@gmail.com>
2026-08-23 22:06:56 +0530

    homework 1&2 are done

---
+++ python1/welcome.py
@@ -0,0 +1,48 @@
+print("Welcome to Python!") #welcome to programming world
+
+
+name="mansukh"
+age=17
+city="pandhro"
+print("my name is",name)
+print("my age is",age)
+print("my city is",city)
+
+print ("hello")
+student_name="mansukh"
+student_roll_number=17
+student_branch="python"
+print("my name is",student_name)
+print("my roll number is",student_roll_number)
+print("my branch is",student_branch)
+
+print("hello, world!")
+marks= 85
+marks= 90
+print("updated marks:", marks)
+
+print("hello, world!")
+marks= 85
+marks= 90
+print("updated marks:", marks)
+
+
+print("hello, world!")
+x = y = z = 0
+print(x, y, z)
+x = y = z = 0
+print(x, y, z)
+
+print("hello, world!")
+student1="Rahul"
+student_name="Rahul"
+college_class="B.Teach"
+print(student1,student_name,college_class)
+
+print("hello, world!")
+student_name="mansukh"
+student_age=17
+student_age="19" 
+
+print("student name:", student_name)
+print(" updated student age:", student_age)
