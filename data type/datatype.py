4fd5d147ea120831abf8b5beb4bc04eedc493943
yash-shingala <yash.shingala.cg@gmail.com>
2026-08-24 17:37:29 +0530

    assignment 3

---
+++ data type/datatype.py
@@ -0,0 +1,77 @@
+age=18
+Age=19.0
+AGE="18"
+AgE=True
+result=None
+print(type(age))
+print(type(Age))
+print(type(AGE))
+print(type(AgE))
+print(type(result))
+
+age=17
+marks="85"
+print(age, type(age))
+print(marks, type(marks))
+
+convert_to_sring=str(age)
+print(convert_to_sring, type(convert_to_sring))
+convert_to_integer=int(marks)
+print(convert_to_integer, type(convert_to_integer)) 
+
+#24
+student_name="raju"
+student_age=18
+student_marks=None
+student_height=5.8
+print(student_name, type(student_name))
+print(student_age, type(student_age))
+print(student_marks, type(student_marks))
+print(student_height, type(student_height))
+
+#25
+a=50
+b=50.0
+c="50"
+print(a, type(a), b, type(b), c, type(c))
+
+#27
+A=None
+B="None"
+print(A, type(A), B, type(B))
+
+#26
+a=True
+b="True"
+print(a, type(a), b, type(b))
+
+#28
+value=8
+print(type(value))
+value="8"
+print(type(value))
+
+#29
+#product details
+product_name="AI"
+product_quantity=1
+product_price=1.1
+product_avabilitiy=True
+product_discount=None
+print(type(product_name), type(product_quantity), type(product_price), type(product_avabilitiy), type(product_discount))
+
+#30
+a=10
+print(type(a))
+b=10.0
+print(type(b))
+c="10"
+print(type(c))
+d=True
+print(type(d))
+e="True"
+print(type(e))
+f=None
+print(type(f))
+g="None"
+print(type(g))
