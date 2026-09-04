58d546025f2689faa9c0ea7897a5d7e8228ea6d7
yash-shingala <yash.shingala.cg@gmail.com>
2026-09-04 13:56:16 +0530

    file of input and output added

---
+++ inputandoutput/inputandoutput.py
@@ -0,0 +1,38 @@
+# name=input("enter your name:")
+# print=(f"my name is {name}")
+# age=int(input("enter your age:"))
+# print=(f"my age is {age}")
+# lab=int(input("enter your lab no.:"))
+# print=(f"my lab no. is {lab}")
+# product_name=input("enter your product name:")
+# print=(f"my product name is {product_name}")
+# product_price=float(input("enter your product price:"))
+# print=(f"my product price is {product_price}")
+# quantity=int(input("enter your quantity:"))
+# print=(f"number of items is {quantity}")
+# total_cost=quantity*product_price   
+# print=(f"total cost is {total_cost}")
+#b=map(int,input("enter your first name and last name:").split())
+
+
+# a,b=input("enter a and b:").split()
+# a=int(a)
+# b=int(b)
+# print(f"first_number:{a} last_name:{b}")
+# print(type(a), type(b))
+
+# a,b=input("enter a and b:").split()
+# print(f"first_number:{int(a)} last_name:{int(b)}")
+# print(type(a),type(b))
+
+# a,b=input("enter a and b:").split()
+# print(f"first_number:{a} last_name:{b}")
+# print(type(a), type(b))
+
+# a,b=map(int,input("enter a and b:").split()[0:2])
+# print(f"first_number:{a} last_name:{b}")  
+
+print("hello", end=" ")
+print("world")
+
+print(26, 27, 28, sep=",")
--- stringoperation/stringoperationpractice.py
+++ stringoperation/stringoperationpractice.py
@@ -1,5 +1,72 @@
-word="python"
-print(word[3])
-print(word[:])
-print(word[3:5])
-print(word[-1:-4])
+# # word="python"
+# # print(word[3])
+# # print(word[:])
+# # print(word[3:5])
+# # print(word[-1:-4])
+
+
+# # a="python"
+# # b="Python"
+# # print(a==b)
+
+# # name="python"
+# # A="pyt"
+# # B="HON"
+# # print(A.upper(),B.lower())
+
+# # name="python"
+# # c=name[:3]
+# # d=name[3:]
+# # print(c.upper(),d.lower())
+
+# # name="python"
+# # e=name[:3].upper()
+# # f=name[3:].lower()
+# # print(e+f)
+
+
+# # q="PYTHON PROGRAMMING &"
+# # print(q.lower())
+# # print(q.casefold())
+
+
+# # f="hello python"
+# # print("python" in f)
+# # print("java" in f)
+# # print("java" not in f)
+# # print(f.find("python"))
+
+
+# # message="Hello Python"
+# # print("python" in message)
+# # print("java" in message)
+# # print("java" not in message)
+# # print(message.find("python"))
+# # print(message.find("java"))
+# # print(message.index("python"))
+# # print(message.startswith("H"))
+# # print(message.startswith("Hello"))
+# # print(message.startswith("P"))
+# # print(message.endswith("Hello"))
+# # print(message.endswith("n"))
+# # print(message.endswith("python"))
+
+
+# text="when gold is old, men is old"
+# print(text.replace("old","new"))
+
+# name="raju"
+# age=20
+# print(f"my name is {name} and my age is {age}.")
+
+# print("hello"=="hello ")
+# print("hello"=="hello ".rstrip())
+
+# print("hello\nworld")
+# print("hello\tworld")
+
+# print("He said \"Hello\"")
+# print('It\'s Python')
+
+# path = r"C:\new\test"
+
